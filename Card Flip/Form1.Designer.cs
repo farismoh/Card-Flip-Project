9c16eaca1f6174868a52cc3fb206153867900ac0
farismoh <farismoh@uab.edu>
2016-05-09 15:39:53 -0500

    Card Flip generation Project

---
+++ Card Flip/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Card_Flip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cardFacePictureBox = new System.Windows.Forms.PictureBox();
+            this.cardBackPictureBox = new System.Windows.Forms.PictureBox();
+            this.showBackButton = new System.Windows.Forms.Button();
+            this.showButtonFace = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFacePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBackPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cardFacePictureBox
+            // 
+            this.cardFacePictureBox.Image = global::Card_Flip.Properties.Resources.Ace_Spades;
+            this.cardFacePictureBox.Location = new System.Drawing.Point(328, 140);
+            this.cardFacePictureBox.Name = "cardFacePictureBox";
+            this.cardFacePictureBox.Size = new System.Drawing.Size(100, 140);
+            this.cardFacePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardFacePictureBox.TabIndex = 1;
+            this.cardFacePictureBox.TabStop = false;
+            this.cardFacePictureBox.Visible = false;
+            // 
+            // cardBackPictureBox
+            // 
+            this.cardBackPictureBox.Image = global::Card_Flip.Properties.Resources.Backface_Blue;
+            this.cardBackPictureBox.Location = new System.Drawing.Point(108, 140);
+            this.cardBackPictureBox.Name = "cardBackPictureBox";
+            this.cardBackPictureBox.Size = new System.Drawing.Size(100, 140);
+            this.cardBackPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardBackPictureBox.TabIndex = 0;
+            this.cardBackPictureBox.TabStop = false;
+            // 
+            // showBackButton
+            // 
+            this.showBackButton.Location = new System.Drawing.Point(90, 354);
+            this.showBackButton.Name = "showBackButton";
+            this.showBackButton.Size = new System.Drawing.Size(140, 75);
+            this.showBackButton.TabIndex = 2;
+            this.showBackButton.Text = "Show the Card Back";
+            this.showBackButton.UseVisualStyleBackColor = true;
+            this.showBackButton.Click += new System.EventHandler(this.showBackButton_Click);
+            // 
+            // showButtonFace
+            // 
+            this.showButtonFace.Location = new System.Drawing.Point(328, 354);
+            this.showButtonFace.Name = "showButtonFace";
+            this.showButtonFace.Size = new System.Drawing.Size(140, 75);
+            this.showButtonFace.TabIndex = 3;
+            this.showButtonFace.Text = "Show the Card Face";
+            this.showButtonFace.UseVisualStyleBackColor = true;
+            this.showButtonFace.Click += new System.EventHandler(this.showButtonFace_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 487);
+            this.Controls.Add(this.showButtonFace);
+            this.Controls.Add(this.showBackButton);
+            this.Controls.Add(this.cardFacePictureBox);
+            this.Controls.Add(this.cardBackPictureBox);
+            this.Name = "Form1";
+            this.Text = "Card Flip";
+            ((System.ComponentModel.ISupportInitialize)(this.cardFacePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBackPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cardBackPictureBox;
+        private System.Windows.Forms.PictureBox cardFacePictureBox;
+        private System.Windows.Forms.Button showBackButton;
+        private System.Windows.Forms.Button showButtonFace;
+    }
+}
+
